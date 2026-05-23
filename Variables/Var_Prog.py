7256fb25cf13cbcb92b193ff06910bb239058d14
Musawer Hussain <musawerhussain10@gmail.com>
2026-05-22 22:56:49 -0700

    Day 1: Variables practice

---
+++ Variables/Var_Prog.py
@@ -0,0 +1,8 @@
+#Create a variable student_name and store any name in it. Print it.
+student_name = "Mudassir"
+print(student_name)
+
+#Store your age in a variable age. Then increase it by 1 and print the new age.
+age = 20
+age += 1
+print(age)
