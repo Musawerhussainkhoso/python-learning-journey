e1db07f5bec33aded7e01ff3d6652e89284259d1
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-06-30 13:06:51 +0500

    Add higher_dimension_array.py to demonstrate creation of 2D and 3D NumPy arrays

---
+++ Python Libraries/Numpy/higher_dimension_array.py
@@ -0,0 +1,21 @@
+import numpy as np
+#Program 1: Create a 2D Array
+import numpy as np
+
+arr = np.array([10, 20, 30, 40], ndmin=2)
+
+print(arr)
+print("Dimensions:", arr.ndim)
+
+#Program 2: Create a 3D Array
+arr = np.array([1, 2, 3, 4], ndmin=3)
+
+print(arr)
+print("Dimensions:", arr.ndim)
+
+#Program 3: Create a 5D Array
+arr = np.array([5, 10, 15], ndmin=5)
+
+print(arr)
+print("Dimensions:", arr.ndim)
+
