a39d854498fcff890bd8e704b8f4d4389b2e332d
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-06-30 20:58:01 +0500

    Add Array_Indexing_1D.py to demonstrate accessing first and last elements in a NumPy array

---
+++ Python Libraries/Numpy/Array_Indexing_1D.py
@@ -0,0 +1,14 @@
+import numpy as np 
+#Access the First Element
+arr = np.array([10, 20, 30, 40, 50])
+print("First Element:", arr[0])
+
+#Access the Last Element
+arr = np.array([5, 10, 15, 20, 25])
+print("Last Element:", arr[-1])
+
+#Access Multiple Elements
+arr = np.array([100, 200, 300, 400, 500])
+print("First Element:", arr[0])
+print("Third Element:", arr[2])
+print("Fifth Element:", arr[4])
