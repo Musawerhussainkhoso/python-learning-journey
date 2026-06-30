617a540c3b8a6578bb45637c6368fea7e14ef96f
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-06-30 21:00:50 +0500

    Enhance Array_Indexing_1D.py to include calculations using indexed elements and print their sum

--- Python Libraries/Numpy/Array_Indexing_1D.py
+++ Python Libraries/Numpy/Array_Indexing_1D.py
@@ -12,3 +12,11 @@ arr = np.array([100, 200, 300, 400, 500])
 print("First Element:", arr[0])
 print("Third Element:", arr[2])
 print("Fifth Element:", arr[4])
+
+#Perform Calculation Using Indexed Elements
+arr = np.array([8, 12, 20, 16, 24])
+# Add first and third elements
+result = arr[0] + arr[2]
+print("First Element:", arr[0])
+print("Third Element:", arr[2])
+print("Sum:", result)
