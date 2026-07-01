fa5f63361a0a64db0d7f1d1759acd03563546a73
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-07-01 15:13:20 +0500

    Add additional examples to Slicing_Array_1D.py demonstrating slicing operations on new 1D NumPy arrays

--- Python Libraries/Numpy/Slicing_Array_1D.py
+++ Python Libraries/Numpy/Slicing_Array_1D.py
@@ -15,3 +15,33 @@ arr = np.array([10,20,30,40,50,60,70,80,90,100])
 print("First 5 elements:", arr[:5])
 print("Last 3 elements:", arr[-3:])
 print("Index 2 to 6:", arr[2:7])
+
+'''
+Create a 1D NumPy array containing numbers 5 to 50 (difference of 5).
+
+Print:
+
+First 4 elements
+Elements from index 3 to the end
+Middle four elements
+'''
+arr = np.array([5,10,15,20,25,30,35,40,45,50])
+
+print("First 4 elements:", arr[:4])
+print("From index 3 to end:", arr[3:])
+print("Middle four elements:", arr[3:7])
+
+'''
+Create a 1D NumPy array containing numbers 100 to 1000 (difference of 100).
+
+Print:
+
+First 6 elements
+Last 5 elements
+Elements from index 1 to index 8
+'''
+arr = np.array([100,200,300,400,500,600,700,800,900,1000])
+
+print("First 6 elements:", arr[:6])
+print("Last 5 elements:", arr[-5:])
+print("Index 1 to 8:", arr[1:9])
