6b5871fb6f4cf2742b4648ad280d5fceaa4eb004
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-07-01 15:20:37 +0500

    Add Slicing_Array_3D.py to demonstrate slicing operations on a 3D NumPy array

---
+++ Python Libraries/Numpy/Slicing_Array_3D.py
@@ -0,0 +1,27 @@
+# Print:
+# 1. Entire Layer 0
+# 2. Entire Layer 1
+# 3. First row from both layers
+# 4. Last column from both layers
+import numpy as np
+
+arr = np.array([
+    [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12]
+    ],
+    [
+        [13,14,15,16],
+        [17,18,19,20],
+        [21,22,23,24]
+    ]
+])
+
+print(arr[0,:,:])
+
+print(arr[1,:,:])
+
+print(arr[:,0,:])
+
+print(arr[:,:,3])
