f40b75d27f6703a9b81353278f9893dc9fe5385e
Musawer Hussain <147225635+Musawerhussainkhoso@users.noreply.github.com>
2026-07-04 20:09:22 +0500

    Add comments and formatting to Pandas_Series.py for clarity and readability

--- Python Libraries/Pandas/Pandas_Series.py
+++ Python Libraries/Pandas/Pandas_Series.py
@@ -1,6 +1,18 @@
 import pandas as pd
 a = [1, 7, 2]
 
-myvar = pd.Series(a)
+myvar = pd.Series(a)#series means one single column 
 
 print(myvar)
+
+#Simple Series Create and Print
+
+numbers = pd.Series([10, 20, 30, 40, 50])
+
+print(numbers)
+
+#Series with Student Names
+
+students = pd.Series(["Ali", "Sara", "Ahmed", "Majid"])
+
+print(students)
