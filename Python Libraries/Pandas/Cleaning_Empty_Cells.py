422db15923c928383b7e1be82f2f3a28de034a50
Musawer Hussain <147225635+Musawerhussainkhoso@users.noreply.github.com>
2026-07-06 10:28:41 +0500

    Add functionality to replace NaN values and save cleaned DataFrame to Excel

--- Python Libraries/Pandas/Cleaning_Empty_Cells.py
+++ Python Libraries/Pandas/Cleaning_Empty_Cells.py
@@ -8,4 +8,8 @@ print(Data)'''
 print(df.head(5))
 print(df.tail(5))
 #Replace empty Values
+df.fillna(0, inplace=True)#replace all NaN values with 0
+print(df)
+df.to_excel(r"D:\Python 2026\python-learning-journey\Python Libraries\Pandas\pandas_practice_dataset(1)_cleaned.xlsx", index=False)
+print("Changes saved successfully in Excel file.")
 
